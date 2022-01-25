919c2f6be09177574901410c885d2ce881db291b
cpanagopoulos <charles.efthimios@gmail.com>
2022-01-24 20:50:54 -0500

    homework update

--- Code/joint_savings.sol
+++ Code/joint_savings.sol
@@ -1,33 +1,30 @@
 pragma solidity ^0.5.0;
 
 // Define a new contract named `JointSavings`
-contract JointSavings {(address payable accountOne, address payable accountTwo);
-    address publiclastToWithdraw;
+contract JointSavings {
+    address payable accountOne;
+    address payable accountTwo;
+    address public lastToWithdraw;
     uint public lastWithdrawAmount;
     uint public contractBalance;
 
-    function withdraw {
-        
-        uint variable amount;
-        payable address recipient;
-
-    }
-    
     function withdraw(uint amount, address payable recipient) public {
 
         
         require(recipient == accountOne || recipient == accountTwo, "You don't own this account!");
 
-        require(address(this).balance >= amount, "Insufficient funds!");
+        require(contractBalance >= amount, "Insufficient funds!");
 
         if (lastToWithdraw != recipient) {
             lastToWithdraw = recipient;
             
-            }
+        }
             
-        function transfer(recipient(lastWithdrawAmount == amount;
+        recipient.transfer(amount);
+        
+        lastWithdrawAmount = amount;
 
-        variable contractBalance = address(this).balance)
+        contractBalance = address(this).balance;
     }
 
     function deposit() public payable {
@@ -41,6 +38,6 @@ contract JointSavings {(address payable accountOne, address payable accountTwo);
 
     }
 
-    function fallback()
+    function() external payable{}
 
 }
